63805c9ca35c4027b83188eacda71c23aeb47541
TonyCrane <975062472@qq.com>
2021-08-01 23:08:10 +0800

    Site updated: 2021-08-01 23:08:10

--- js/main.js
+++ js/main.js
@@ -139,8 +139,8 @@
 $(function () {
     Array.from($("img")).forEach(img => {
         let ori = img.src;
-        if (ori.match(/https:\/\/blog\.tonycrane\.cc\/.*?\.(jpg|png)/)) {
-            img.src = "https://cdn.jsdelivr.net/gh/tonycrane/" + ori.substr(8);
+        if (ori.match(/https:\/\/blog\.tonycrane\.cc\/.*?\.(jpg|png|gif)/)) {
+            img.src = "https://cdn.jsdelivr.net/gh/tonycrane/tonycrane.github.io/" + ori.substr(26);
         }
     });
 });
